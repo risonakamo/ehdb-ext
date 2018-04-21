c6558f32888a5926028baa329b4bb43f5c4734a8
risonakamo <hmykanae@gmail.com>
2018-04-21 17:36:31 -0400

    now outputs in json counter for number of sites read

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,31 @@
 window.onload=main;
 
+var _tabcount={known:0,unknown:0};
+
 function main()
+{
+    chrome.tabs.query({currentWindow:true},(tabs)=>{
+        var res=[];
+        for (var x=0,l=tabs.length;x<l;x++)
+        {
+            res.push(processTabjson(tabs[x]));
+        }
+
+        document.querySelector(".result").innerText=JSON.stringify(res);
+
+        var infoline=document.querySelector(".info");
+        var infolinestring=`found ${_tabcount.known} known site(s)`;
+        if (_tabcount.unknown>0)
+        {
+            infolinestring+=`\nfound ${_tabcount.unknown} unknown site(s)`;
+        }
+        infolinestring+="\n---------\n";
+        infoline.innerText=infolinestring;
+    });
+}
+
+//old main returned output as new line strings
+function mainold()
 {
     chrome.tabs.query({currentWindow:true},(tabs)=>{
         var res="";
@@ -10,6 +35,15 @@ function main()
         }
 
         document.querySelector(".result").innerText=res;
+
+        var infoline=document.querySelector(".info");
+        var infolinestring=`found ${_tabcount.known} known site(s)`;
+        if (_tabcount.unknown>0)
+        {
+            infolinestring+=`\nfound ${_tabcount.unknown} unknown site(s)`;
+        }
+        infolinestring+="\n---------\n";
+        infoline.innerText=infolinestring;
     });
 }
 
@@ -22,6 +56,8 @@ function main()
 function processTab(tab)
 {
     var res="";
+    _tabcount.known+=1;
+
     if (tab.url.slice(0,19)=="https://nhentai.net")
     {
         res+=tab.title.replace(/.*: (.+) ».*/,"$1");
@@ -50,9 +86,54 @@ function processTab(tab)
     {
         res+=tab.title;
         res+=`\nn/a`;
+        _tabcount.known-=1;
+        _tabcount.unknown+=1;
     }
 
     res+=`\n${tab.url}\n---\n`;
+
+    return res;
+}
+
+//process tab and return js object data
+function processTabjson(tab)
+{
+    var res={};
+    _tabcount.known+=1;
+
+    if (tab.url.slice(0,19)=="https://nhentai.net")
+    {
+        res.title=tab.title.replace(/.*: (.+) ».*/,"$1");
+        res.type="manga/group";
+    }
+
+    else if (tab.url.slice(0,31)=="https://chan.sankakucomplex.com")
+    {
+        res.title=tab.title.replace(/(.+) \|.*/,"$1");
+        res.type="image/group";
+    }
+
+    else if (tab.url.slice(0,21)=="https://www.pixiv.net")
+    {
+        res.title=tab.title.replace(/「(.+)」.*/,"$1");
+        res.type="pixiv";
+    }
+
+    else if (tab.url.slice(0,17)=="https://hitomi.la")
+    {
+        res.title=tab.title.replace(/(.+) \|.*/,"$1");
+        res.type="cg/group";
+    }
+
+    else
+    {
+        res.title=tab.title;
+        res.type="n/a";
+        _tabcount.known-=1;
+        _tabcount.unknown+=1;
+    }
+
+    res.url=tab.url;
 
     return res;
 }
