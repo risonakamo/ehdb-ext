e792807e952609a955048c80c4c771f2e0a65a6d
risonakamo <hmykanae@gmail.com>
2018-04-21 17:10:29 -0400

    regex titles for supported sites

--- popup/popup.js
+++ popup/popup.js
@@ -13,27 +13,46 @@ function main()
     });
 }
 
+/*output order:
+  title
+  type string
+  url
+  ---
+*/
 function processTab(tab)
 {
+    var res="";
     if (tab.url.slice(0,19)=="https://nhentai.net")
     {
-        return `${tab.title}\nimage/group\n${tab.url}\n---\n`;
+        res+=tab.title.replace(/.*: (.+) ».*/,"$1");
+        res+=`\nmanga/group`;
     }
 
     else if (tab.url.slice(0,31)=="https://chan.sankakucomplex.com")
     {
-        return `${tab.title}\nimage/group\n${tab.url}\n---\n`;
+        res+=tab.title.replace(/(.+) \|.*/,"$1");
+        res+=`\nimage/group`;
     }
 
     else if (tab.url.slice(0,21)=="https://www.pixiv.net")
     {
-        return `${tab.title}\npixiv\n${tab.url}\n---\n`;
+        res+=tab.title.replace(/「(.+)」.*/,"$1");
+        res+=`\npixiv`;
     }
 
     else if (tab.url.slice(0,17)=="https://hitomi.la")
     {
-        return `${tab.title}\ncg/group\n${tab.url}\n---\n`;
+        res+=tab.title.replace(/(.+) \|.*/,"$1");
+        res+=`\ncg/group`;
     }
 
-    return `${tab.title}\nunsupported site\n${tab.url}\n---\n`;
+    else
+    {
+        res+=tab.title;
+        res+=`\nn/a`;
+    }
+
+    res+=`\n${tab.url}\n---\n`;
+
+    return res;
 }
