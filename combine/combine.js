82d23ef93b86e07d0b4f685c23bf023655b553ff
risonakamo <hmykanae@gmail.com>
2018-04-21 18:48:39 -0400

    main combine function working

--- combine/combine.js
+++ combine/combine.js
@@ -4,3 +4,30 @@ function main()
 {
 
 }
+
+function combine()
+{
+    var gendata=JSON.parse(document.querySelector(".gen-input").value);
+    var imgs=document.querySelector(".imgs").value.split("\n");
+    var tags=document.querySelector(".tags").value.split("\n");
+
+    //could make this better
+    if (gendata.length!=imgs.length || gendata.length!=tags.length)
+    {
+        console.warn("data length mismatch");
+        console.group("data counts");
+        console.warn(`gendata: ${gendata.length}`);
+        console.warn(`imgs: ${imgs.length}`);
+        console.warn(`tags: ${tags.length}`);
+        console.groupEnd();
+        return;
+    }
+
+    var res="";
+    for (var x=0,l=gendata.length;x<l;x++)
+    {
+        res+=`${gendata[x].title}\n${gendata[x].type}\n${imgs[x]}\n${gendata[x].url}\n${tags[x]}\n------\n`;
+    }
+
+    return res;
+}
