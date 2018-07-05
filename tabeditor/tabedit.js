4b11d6da5d62fe02083b1847394d21573aec21de
risonakamo <hmykanae@gmail.com>
2018-07-05 14:07:57 -0400

    refs saving for output

--- tabeditor/tabedit.js
+++ tabeditor/tabedit.js
@@ -28,22 +28,45 @@ class EntryHandler extends React.Component
     constructor(props)
     {
         super(props);
+        this.madeEntry=this.madeEntry.bind(this);
+        this.getOutput=this.getOutput.bind(this);
+
+        this.entries=[];
+        this.element=React.createRef();
+    }
+
+    madeEntry(ref)
+    {
+        this.entries.push(ref);
+    }
+
+    getOutput()
+    {
+        for (var x=0;x<this.entries.length;x++)
+        {
+            console.log(this.entries[x].genOutput());
+        }
+    }
+
+    componentDidMount()
+    {
+        this.element.current.getOutput=this.getOutput;
     }
 
     render()
     {
         return React.createElement(
             "div",
-            null,
+            {ref:this.element},
 
             this.props.data.map((x,i)=>{
-                return React.createElement(Entry,{data:x,key:i+1,index:i+1});
+                return React.createElement(Entry,{data:x,key:i+1,index:i+1,ref:this.madeEntry});
             })
         );
     }
 }
 
-//Entry(object data,int key,int index)
+//Entry(object data,int key,int index,element nextEntry)
 //give it a single data object and ints key and index, which are just
 //the number that goes on the left
 class Entry extends React.Component
@@ -51,6 +74,27 @@ class Entry extends React.Component
     constructor(props)
     {
         super(props);
+        this.genOutput=this.genOutput.bind(this);
+        this.focusTextBox=this.focusTextBox.bind(this);
+
+        this.entryFields=[React.createRef(),React.createRef(),React.createRef()];
+    }
+
+    //return output data of this entry
+    genOutput()
+    {
+        var data=this.props.data;
+        data.title=this.entryFields[0].current.innerHTML;
+        data.imglink=this.entryFields[1].current.innerHTML;
+        data.tags=this.entryFields[2].current.innerHTML;
+
+        return data;
+    }
+
+    //focus one of the 3 input boxes, given the index
+    focusTextBox(boxnum)
+    {
+        this.entryFields[boxnum].current.focus();
     }
 
     render()
@@ -58,7 +102,7 @@ class Entry extends React.Component
         return React.createElement(
             "div",
             {
-                className: `entry ${this.props.data.type}`
+                className:`entry ${this.props.data.type}`
             },
 
             React.createElement(
@@ -79,18 +123,18 @@ class Entry extends React.Component
 
                 React.createElement(
                     "dd",
-                    {className:"title",contentEditable:""},
+                    {className:"title",contentEditable:"",ref:this.entryFields[0]},
                     this.props.data.title
                 ),
 
                 React.createElement(
                     "dd",
-                    {className:"img-link",contentEditable:""}
+                    {className:"img-link",contentEditable:"",ref:this.entryFields[1]}
                 ),
 
                 React.createElement(
                     "dd",
-                    {className:"tags",contentEditable:""}
+                    {className:"tags",contentEditable:"",ref:this.entryFields[2]}
                 )
             )
         );
