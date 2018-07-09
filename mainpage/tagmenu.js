99940d603b30ebac04257b018972c40c543bf9f0
risonakamo <hmykanae@gmail.com>
2018-07-08 20:59:13 -0400

    tag selected visual change

--- mainpage/tagmenu.js
+++ mainpage/tagmenu.js
@@ -20,11 +20,37 @@ class TagMenu extends React.Component
 //TagMenuTag(string tagname,int amount)
 class TagMenuTag extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.toggleSelected=this.toggleSelected.bind(this);
+
+        this.state={
+            //selected:null*
+        };
+    }
+
+    toggleSelected()
+    {
+        if (!this.state.selected)
+        {
+            this.setState({selected:"selected"});
+        }
+
+        else
+        {
+            this.setState({selected:null});
+        }
+    }
+
     render()
     {
         return React.createElement(
             "li",
-            null,
+            {
+                className:this.state.selected,
+                onClick:this.toggleSelected
+            },
 
             React.createElement("span",null,
                 React.createElement("div",{className:"amount-label"},this.props.amount),
